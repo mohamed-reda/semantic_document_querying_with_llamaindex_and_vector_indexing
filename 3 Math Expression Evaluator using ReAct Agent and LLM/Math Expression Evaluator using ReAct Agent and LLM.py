7850860b6446e360499969151243a8889891a45a
mohamed-reda <mohamed.reda.007007@gmail.com>
2025-02-20 14:38:25 +0200

    organize the code

--- 3 Math Expression Evaluator using ReAct Agent and LLM/Math Expression Evaluator using ReAct Agent and LLM.py
+++ 3 Math Expression Evaluator using ReAct Agent and LLM/Math Expression Evaluator using ReAct Agent and LLM.py
@@ -8,25 +8,6 @@ setup_models()
 
 
 # --------
-def multiply(a: float, b: float) -> float:
-    """Multiply two numbers and returns the product"""
-    return a * b
-
-
-def add(a: float, b: float) -> float:
-    """Add two numbers and returns the sum"""
-    return a + b
-
-
-def subtract(a: float, b: float) -> float:
-    """Subtract two numbers and returns the difference"""
-    return a - b
-
-
-def divide(a: float, b: float) -> float:
-    """Divide two numbers and returns the quotient"""
-    return a / b
-
 
 def multiply(a: float, b: float) -> float:
     """Multiply two numbers and returns the product"""
